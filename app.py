846e5981f5a5d125c46308adb82093005e6ff4b7
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-09-12 10:31:34 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
-print("""
+import os
 
-ℂ𝕒𝕥𝕒𝕣𝕒𝕥𝕒𝕤 ℕ𝕒𝕥𝕒𝕔̧𝕒̃𝕠
+def mostra_titulo():
+    print('''
 
-""")
+    ℂ𝕒𝕥𝕒𝕣𝕒𝕥𝕒𝕤 ℕ𝕒𝕥𝕒𝕔̧𝕒̃𝕠
 
-print('1. Cadastro de nadadores')
-print('2. Listar nadadores')
-print('3. Ativar nadador')
-print('4. Sair da aplicação')
+    ''')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-print('Você escolheu a opção: ', opcao_escolhida)
+def mostra_escolhas():
+    print('1. Cadastro de nadadores')
+    print('2. Listar nadadores')
+    print('3. Ativar nadador')
+    print('4. Sair da aplicação')
 
-if opcao_escolhida == 1:
-    print('Cadastrar nadador')
-elif opcao_escolhida == 2:
-    print('Listar Nadadores')
-elif opcao_escolhida == 3:
-    print('Ativar/desativar nadador')
-else:
-    print('Saindo da aplicação')
+def escolhe_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print('Você escolheu a opção: ', opcao_escolhida)
+
+    def finalizar_programa():
+        os.system('clear')
+        print('Finalizando programa')
+
+
+    if opcao_escolhida == 1:
+        print('Cadastrar nadador')
+    elif opcao_escolhida == 2:
+        print('Listar Nadadores')
+    elif opcao_escolhida == 3:
+        print('Ativar/desativar nadador')
+    else:
+        finalizar_programa()
+
+def main():
+    mostra_titulo()
+    mostra_escolhas()
+    escolhe_opcao()
+
+if __name__ == '__main__':
+    main()
